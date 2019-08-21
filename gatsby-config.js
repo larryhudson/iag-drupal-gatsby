2434f862e8b81b8bd9393075afe76664c8136eb8
Larry Hudson <harryludson@gmail.com>
2019-08-21 17:47:44 +1000

    added article template and stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-drupal",
+      options: {
+        baseUrl: "http://dev-larryhudson-gatsby.pantheonsite.io/",
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
---
+++ src/templates/article.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
+
+export const query = graphql`
+  query Article($articleId: String!) {
+    nodeArticle(id: { eq: $articleId }) {
+      title
+      body {
+        processed
+      }
+      field_image {
+        alt
+      }
+      relationships {
+        field_image {
+          localFile {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const ArticlePage = ({ data }) => {
+  const article = {
+    title: data.nodeArticle.title,
+    body: data.nodeArticle.body.processed,
+    imageData:
+      data.nodeArticle.relationships.field_image.localFile.childImageSharp
+        .fluid,
+    imageAlt: data.nodeArticle.field_image.alt,
+  }
+
+  return <Article {...article} />
+}
+
+const Article = ({ title, body, imageData, imageAlt }) => {
+  return (
+    <div style={{ maxWidth: "48em", margin: "0 auto" }}>
+      <h1>{title}</h1>
+      <Image fluid={imageData} alt={imageAlt} />
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+    </div>
+  )
+}
+
+export default ArticlePage
